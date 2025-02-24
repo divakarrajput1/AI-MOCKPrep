c6f9dd42a12f93e38877ddabc39d700b1d4a7405
Divakar <divakarrajput5818@gmail.com>
2025-02-25 05:18:22 +0530

    Initial Commit

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.js",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_b78oXxYCZaTK@ep-frosty-river-a8ryf39k-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+  },
+};
